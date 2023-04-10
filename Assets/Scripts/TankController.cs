b49d1f087feb0f466c900f6efb8c0a7d7088eca9
Salvador Banderas Rovira <info@salvadorbanderas.eu>
2023-04-10 06:25:51 +0200

    Implemented challenge 1

---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Mirror;
+
+public class TankController : NetworkBehaviour
+{
+    private void Update()
+    {
+        if (!isLocalPlayer) {
+            return;
+        }
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
